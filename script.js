8a5c67347fdd0e9986ef112f0b19bb8c38ef8c9d
Tillsxp <matilda.89@live.se>
2022-09-30 06:19:34 +0200

    Kommentarer & Css finslipning

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let knapp = document.createElement("button"); // Skapar en knapp
+let knapp = document.createElement("button"); // Skapar en knapp.
 let knappText = document.createTextNode("Logga ut");
 
 let userInput = document.getElementById("userInput");
@@ -6,19 +6,19 @@ let passInput = document.getElementById("passInput");
 let loginKnapp = document.getElementById("click");
 
 
-if (localStorage.getItem("namn") == "Sara") { // Kollar om användaren är inloggad sen tidigare
+if (localStorage.getItem("namn") == "Sara") { // Kollar om användaren är inloggad sen tidigare.
 
 document.getElementById("login").style.display="none";
 logoutKnapp();
 }
  
-    function logoutKnapp () {  // Skapar en logga ut knapp
+    function logoutKnapp () {  // Skapar en logga ut knapp.
 
         knapp.appendChild(knappText);
         document.body.appendChild(knapp);
     } 
     
-    knapp.addEventListener("click", function (){ // Loggar ut när knappen trycks
+    knapp.addEventListener("click", function (){ // Loggar ut när knappen trycks.
 
         document.getElementById("login").style.display="flex";
         localStorage.clear();
@@ -35,7 +35,7 @@ loginKnapp.addEventListener("click", function() { // Vid login lägger in använ
         document.getElementById("login").style.display = "none";
         alert("Välkommen,du är nu inloggad.");
         logoutKnapp();
-    } else {
+    } else { // Om användarinfo är fel visas ett meddelande.
         
         alert("Användarnamnet eller lösenordet är inkorrekt.");
     }
